c06c496c25b22962e499c7dc34ebbdc9379ad3da
Filip Tangen <filiptangen1998@live.com>
2019-05-02 21:11:09 +0200

    Made it so that youtube autoplay is always off

--- BattleAgainstTheAlgorithms/youtube.js
+++ BattleAgainstTheAlgorithms/youtube.js
@@ -35,6 +35,14 @@ function execute() {
     let names = name.split(".");
     if (names[1] === "youtube") {
 
+        //Clicks the autoplay button such that it is always off.
+        try{
+            let clicker = document.getElementById('toggle');
+            if (clicker.active === true) {
+                clicker.click();
+            }
+        }catch(err){}
+
         //Removes the videoes on the suggested bar
         try {
             document.getElementById("related").style.display = 'none';
@@ -45,6 +53,7 @@ function execute() {
         } catch (err) {
         }
 
+        /*When autoplay is off, youtube gives a lot of suggested videos in the end of the video. This removes them*/
         try {
             let els3 = document.getElementsByClassName("ytp-endscreen-content");
             Array.prototype.forEach.call(els3, function (el) {
